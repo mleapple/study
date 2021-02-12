b743bb0eccca685c8f2512e459524f7dc3cac2a9
mleapple <aegis__17l@naver.com>
2021-02-12 21:05:48 +0900

    커밋테스트

--- src/main/java/com/study/jpa/TestMain.java
+++ src/main/java/com/study/jpa/TestMain.java
@@ -5,6 +5,9 @@ public class TestMain {
 	public static void main(String[] args) {
 		// TODO Auto-generated method stub
 		System.out.println("test");
+		System.out.println("test");
+		System.out.println("test");
+		
 	}
 
 }
